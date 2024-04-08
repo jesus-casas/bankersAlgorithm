23bf1917a797d6b1ef5b5bb606a034395b288497
jesus-casas <javiercasas740@gmail.com>
2024-04-08 00:53:39 -0700

    finished the enterClaimGraph

--- main.c
+++ main.c
@@ -17,6 +17,69 @@ void releaseResource();
 void determineSafeSequence();
 void quitProgram();
 
+void enterClaimGraph() {
+    printf("Enter number of resources: ");
+    scanf("%d", &numResources);
+
+    // Allocate memory for resource array
+    resource = (int *)malloc(numResources * sizeof(int));
+
+    printf("Enter number of units for resources (r0 to r%d): ", numResources - 1);
+    for (int i = 0; i < numResources; i++) {
+        scanf("%d", &resource[i]);
+    }
+
+    printf("Enter number of processes: ");
+    scanf("%d", &numProcesses);
+
+    // Allocate memory for available, maxClaim, allocated, and need
+    available = (int *)malloc(numResources * sizeof(int));
+    maxClaim = (int **)malloc(numProcesses * sizeof(int *));
+    allocated = (int **)malloc(numProcesses * sizeof(int *));
+    need = (int **)malloc(numProcesses * sizeof(int *));
+
+    for (int i = 0; i < numProcesses; i++) {
+        maxClaim[i] = (int *)malloc(numResources * sizeof(int));
+        allocated[i] = (int *)malloc(numResources * sizeof(int));
+        need[i] = (int *)malloc(numResources * sizeof(int));
+    }
+
+    // Initialize available array with resource values
+    for (int i = 0; i < numResources; i++) {
+        available[i] = resource[i];
+    }
+}
+
+void requestResource() {
+    // Implement this function
+}
+
+void releaseResource() {
+    // Implement this function
+}
+
+void determineSafeSequence() {
+    // Implement this function
+}
+
+void quitProgram() {
+    // Free dynamically allocated memory
+    free(resource);
+    free(available);
+    for (int i = 0; i < numProcesses; i++) {
+        free(maxClaim[i]);
+        free(allocated[i]);
+        free(need[i]);
+    }
+    free(maxClaim);
+    free(allocated);
+    free(need);
+
+    printf("Quitting program...\n");
+}
+
+
+
 int main() {
     int choice;
 
@@ -54,36 +117,4 @@ int main() {
     }
 
     return 0;
-}
-
-void enterClaimGraph() {
-    // Implement this function
-}
-
-void requestResource() {
-    // Implement this function
-}
-
-void releaseResource() {
-    // Implement this function
-}
-
-void determineSafeSequence() {
-    // Implement this function
-}
-
-void quitProgram() {
-    // Free dynamically allocated memory
-    free(resource);
-    free(available);
-    for (int i = 0; i < numProcesses; i++) {
-        free(maxClaim[i]);
-        free(allocated[i]);
-        free(need[i]);
-    }
-    free(maxClaim);
-    free(allocated);
-    free(need);
-
-    printf("Quitting program...\n");
 }
